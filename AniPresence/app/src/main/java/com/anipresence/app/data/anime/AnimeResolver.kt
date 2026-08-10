988ff9dc2a7938ea69a134d7976406dfa6b3b782
Eros <131939456+sunken000@users.noreply.github.com>
2026-08-10 02:51:43 -0300

    Remove AniPresence do repositório

--- AniPresence/app/src/main/java/com/anipresence/app/data/anime/AnimeResolver.kt
+++
@@ -1,8 +0,0 @@
-package com.anipresence.app.data.anime
-
-import com.anipresence.app.domain.model.AnimeMatch
-import com.anipresence.app.domain.model.ParsedMediaTitle
-
-interface AnimeResolver {
-    suspend fun resolve(input: ParsedMediaTitle): AnimeMatch?
-}
